83a65f13490041469f3b470aab6f4c5740813a0a
bskdevops <balamuralikrishna.nagarajan@ust-global.com>
2020-02-21 20:43:55 +0530

    first commit devopsproject

---
+++ demoworkingcopy.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class demoworkingcopy
+{
+	public static void main(String args[])
+	{
+		System.out.println("Demo Working Copy");
+	}
+}
---
+++ demoworkingstatus.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class demoworkingstatus
+{
+	public static void main(String args[])
+	{
+		System.out.println("Demo Working Status");
+	}
+}
